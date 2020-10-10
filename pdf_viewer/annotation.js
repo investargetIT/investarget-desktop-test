dc8a7016f55737e7092fa86c4b02eb8c1462a8d8
Investarget <system@investarget.com>
2020-10-10 11:00:14 +0800

    Build for test

--- pdf_viewer/annotation.js
+++ pdf_viewer/annotation.js
@@ -1,18 +1,142 @@
 const { default: PDFJSAnnotate } = PDFAnnotate;
 const { UI, config: { annotationLayerName } } = PDFJSAnnotate;
 
-const documentId = 'test.pdf';
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const dataroomId = urlParams.get('dataroomId');
+const fileId = urlParams.get('fileId');
+
+const documentId = fileId;
+
+const baseUrl = 'http://apitest.investarget.com';
 
 let submitComment = false;
 let isReply = false;
 
-const loadAllComments = function () {
+// TODO: get all annotations adding page_size
+const getAnnotationsReq = async (documentId) => {
+  const user = getUserInfo()
+  if (!user) {
+    throw new Error('user missing');
+  }
+
+  const source = parseInt(localStorage.getItem('source'), 10)
+  if (!source) {
+    throw new Error('data source missing');
+  }
+
+  const reqDiscussion = await fetch(`${baseUrl}/dataroom/discuss/?file=${documentId}&page_size=1`, {
+    method: 'GET',
+    credentials: 'include',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'clienttype': '3',
+      'source': source,
+      'token': user.token,
+    },
+  });
+  const response = await reqDiscussion.json();
+  const { code } = response;
+  if (code !== 1000) {
+    if (response.errormsg) {
+      alert(response.errormsg);
+    } else {
+      alert('未知错误');
+    }
+    return;
+  }
+  const { result: { count, data } } = response;
+  if (count <= 1) {
+    return data;
+  }
+  const reqDiscussion2 = await fetch(`${baseUrl}/dataroom/discuss/?file=${documentId}&page_size=${count}`, {
+    method: 'GET',
+    credentials: 'include',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'clienttype': '3',
+      'source': source,
+      'token': user.token,
+    },
+  });
+  const response2 = await reqDiscussion2.json();
+  return response2.result.data;
+}
+
+const saveAnnotationsToLocalStorage = async (documentId) => {
+  const data = await getAnnotationsReq(documentId);
+  const annotations = data.map(m => {
+    const annotation = JSON.parse(m.location);
+    return { ...annotation, systemId: m.id };
+  });
+  localStorage.setItem(`${documentId}/annotations`, JSON.stringify(annotations));
+}
+
+saveAnnotationsToLocalStorage(documentId);
+
+const myStoreAdapter = new PDFJSAnnotate.StoreAdapter({
+  getAnnotations(documentId, pageNumber) {
+    return getAnnotationsForAdapter(documentId, pageNumber);
+  },
+  addAnnotation(documentId, pageNumber, annotation) {
+    return addAnnotationReq(documentId, pageNumber, annotation);
+  },
+  getComments() {
+    return new Promise((resolve) => {
+      return resolve([]);
+    });
+  },
+  // getAnnotation(documentId, annotationId) {/* ... */},
+
+  // editAnnotation(documentId, pageNumber, annotation) {/* ... */},
+
+  // deleteAnnotation(documentId, annotationId) {/* ... */},
+  
+  // addComment(documentId, annotationId, content) {/* ... */},
+
+  // deleteComment(documentId, commentId) {/* ... */}
+});
+
+// PDFJSAnnotate.setStoreAdapter(myStoreAdapter);
+PDFJSAnnotate.setStoreAdapter(new PDFJSAnnotate.LocalStoreAdapter());
+
+const loadAllComments = async function () {
   const generateSingleComment = function (annotation) {
-    const { uuid: annotationId, page, comments } = annotation;
-    const commentHTML = comments.map(m => `<div class="comment-content">${m.content}</div>`).reduce((prev, curr) => prev + curr, '');
-    return `<div class="comment-container" data-annotation-uuid="${annotationId}">
-      <div class="comment-page">Page ${page}</div>  
-      ${commentHTML} 
+    const {
+      uuid: annotationId,
+      page,
+      question,
+      user,
+      asktime,
+      id: systemId,
+      answer,
+      answertime,
+      trader,
+    } = annotation;
+    let answerHTML = '';
+    if (answer && answertime && trader) {
+      answerHTML = `<div class="comment-wrapper">
+        <img class="comment-author-avatar" src="${trader.photourl}" />
+        <div class="comment-right">
+          <div class="comment-time">回复于 ${answertime.slice(0, 16).replace('T', ' ')}</div>
+          <div class="comment-author-name">${trader.username}</div>
+          <div class="comment-content">${answer}</div>
+        </div>
+      </div>`;
+    }
+    return `<div class="comment-container" data-annotation-uuid="${annotationId}" data-annotation-system-id="${systemId}">
+      <div class="comment-page">Page ${page}</div>
+      <div class="comment-wrapper">
+        <img class="comment-author-avatar" src="${user.photourl}" />
+        <div class="comment-right">
+          <div class="comment-time">提问于 ${asktime.slice(0, 16).replace('T', ' ')}</div>
+          <div class="comment-author-name">${user.username}</div>
+          <div class="comment-content">${question}</div>
+        </div>
+      </div>
+      ${answerHTML} 
       <div class="comment-actions">
         <img class="comment-actions__icon comment-actions__reply" src="/pdf_viewer/images/annotationBarButton-reply.png" />
         <img class="comment-actions__icon comment-actions__delete" src="/pdf_viewer/images/annotationBarButton-delete.png" />
@@ -20,18 +144,26 @@ const loadAllComments = function () {
     </div>`
   };
   const commentsView = document.getElementById('commentsView');
-  const annotationStr = localStorage.getItem(`${documentId}/annotations`);
-  const allAnnotations = JSON.parse(annotationStr);
+  // const annotationStr = localStorage.getItem(`${documentId}/annotations`);
+  // const allAnnotations = JSON.parse(annotationStr);
+  const allAnnotations = await getAnnotationsReq(documentId);
+  console.log('all annotations', allAnnotations);
   let annotationComments = [];
   if (allAnnotations) {
-    annotationComments = allAnnotations.filter(f => f.class === 'Annotation');
-    annotationComments = annotationComments.map(m => {
-      const comments = allAnnotations.filter(f => f.class === 'Comment' && f.annotation === m.uuid);
-      return { ...m, comments };
+    // annotationComments = allAnnotations.filter(f => f.class === 'Annotation');
+    annotationComments = allAnnotations.filter(f => f.location).map(m => {
+      const { location } = m;
+      const annotation = JSON.parse(location);
+      const { page, uuid } = annotation;
+      return { ...m, page, uuid };
     });
+    // annotationComments = annotationComments.map(m => {
+    //   const comments = allAnnotations.filter(f => f.class === 'Comment' && f.annotation === m.uuid);
+    //   return { ...m, comments };
+    // });
   }
   console.log('annotation comments', annotationComments);
-  const commentsHTML = annotationComments.filter(f => f.comments.length > 0)
+  const commentsHTML = annotationComments.filter(f => f.question && f.page)
     .map(m => generateSingleComment(m))
     .reduce((previous, current) => previous + current, '');
   if (commentsHTML) {
@@ -56,18 +188,21 @@ const loadAllComments = function () {
       UI.createEditOverlay(target);
     }
   });
-  $('.comment-actions__reply').click(function() {
 
+  // 回复评论
+  $('.comment-actions__reply').click(function() {
     UI.disableEdit();
     // disable point here otherwise you can't focus when replay a comment
     disablePoint();
 
-    const annotationId = $(this).parents('.comment-container').attr('data-annotation-uuid');
-    $('#add-comment-form').data('annotation', { documentId, annotationId });
+    const annotationId = $(this).parents('.comment-container').attr('data-annotation-system-id');
+    $('#add-comment-form').data('replyAnnotation', { documentId, annotationId });
     isReply = true;
     $('#add-comment-form').modal();
     return false;
   });
+  
+  // 删除评论
   $('.comment-actions__delete').click(function() {
     if (!window.confirm('你确定要删除这些评论吗？相关的批注也会被一并删除')) return false;
     
@@ -76,9 +211,9 @@ const loadAllComments = function () {
     UI.disableEdit();
 
     const annotationId = $(this).parents('.comment-container').attr('data-annotation-uuid');
-    // Fisrt delete all comments
-    const relatedComments = allAnnotations.filter(f => f.class === 'Comment' && f.annotation === annotationId);
-    Promise.all(relatedComments.map(m => PDFJSAnnotate.getStoreAdapter().deleteComment(documentId, m.uuid))).then(() => {
+    const systemId = $(this).parents('.comment-container').attr('data-annotation-system-id');
+
+    deleteAnnotationReq(systemId).then(res => {
       loadAllComments();
       PDFJSAnnotate.getStoreAdapter().deleteAnnotation(documentId, annotationId).then(() => {
         // TODO: refactor codes below
@@ -117,9 +252,7 @@ const drawAnnotationLayer = function (page) {
   svgLayer.setAttribute('class', `${annotationLayerName} custom-annotation-layer`);
   pageHtml.insertBefore(svgLayer, pageHtml.children[1]);
   
-  PDFJSAnnotate.setStoreAdapter(new PDFJSAnnotate.LocalStoreAdapter());
-  const adapter = PDFJSAnnotate.getStoreAdapter();
-  adapter.getAnnotations(documentId, pageNumber ).then(annotations => {
+  PDFJSAnnotate.getStoreAdapter().getAnnotations(documentId, pageNumber ).then(annotations => {
     const svg = document.querySelector(`.page[data-page-number="${pageNumber}"] .custom-annotation-layer`);
     svg.setAttribute('width', viewport.width);
     svg.setAttribute('height', viewport.height);
@@ -234,7 +367,7 @@ UI.addEventListener('annotation:add', (documentId, pageNumber, annotation) => {
     }, 1000);
     return;
   }
-  $('#add-comment-form').data('annotation', { documentId, annotationId: annotation.uuid });
+  $('#add-comment-form').data('formAnnotation', { documentId, pageNumber, annotation });
   $('#add-comment-form').modal();
 });
 
@@ -247,7 +380,7 @@ UI.addEventListener('annotation:delete', (documentId, annotationId) => {
   });
 });
 
-// 提交评论
+// 提交评论或者回复
 $('#comment-submit-button').click(function(e) {
   e.preventDefault();
   const content = $('#comment-content').val();
@@ -255,15 +388,30 @@ $('#comment-submit-button').click(function(e) {
     alert('评论内容不能为空');
     return
   };
-  const annotation = $('#add-comment-form').data('annotation');
-  const { documentId, annotationId } = annotation;
-  PDFJSAnnotate.getStoreAdapter().addComment(documentId, annotationId, content).then(comment => {
-    $('#add-comment-form').removeData('annotation');
-    submitComment = true;
-    $.modal.close();
-    $('#comment-content').val('');
-    loadAllComments();
-  });
+  if (isReply) {
+    const replyData = $('#add-comment-form').data('replyAnnotation');
+    const { documentId, annotationId } = replyData;
+    console.log('docu id', documentId);
+    console.log('annotation id', annotationId);
+    addAnnotationCommentReq(annotationId, content).then(() => {
+      $('#add-comment-form').removeData('replyAnnotation');
+      submitComment = true;
+      $.modal.close();
+      $('#comment-content').val('');
+      loadAllComments();
+    });
+  } else {
+    const formAnnotation = $('#add-comment-form').data('formAnnotation');
+    const { documentId, pageNumber, annotation } = formAnnotation;
+    addAnnotationReq(documentId, pageNumber, annotation, content).then(() => {
+      $('#add-comment-form').removeData('formAnnotation');
+      submitComment = true;
+      $.modal.close();
+      $('#comment-content').val('');
+      loadAllComments();
+      saveAnnotationsToLocalStorage(documentId);
+    });
+  }
 });
 
 $('#add-comment-form').on($.modal.BEFORE_CLOSE, function(event, modal) {
@@ -271,10 +419,10 @@ $('#add-comment-form').on($.modal.BEFORE_CLOSE, function(event, modal) {
   console.log('modal', modal);
   if (!submitComment && !isReply) {
     $('#comment-content').val('');
-    const annotation = $('#add-comment-form').data('annotation');
-    const { documentId, annotationId } = annotation;
+    const formAnnotation = $('#add-comment-form').data('formAnnotation');
+    const { documentId, annotation: { uuid: annotationId } } = formAnnotation;
     PDFJSAnnotate.getStoreAdapter().deleteAnnotation(documentId, annotationId).then(() => {
-      $('#add-comment-form').removeData('annotation');
+      $('#add-comment-form').removeData('formAnnotation');
 
       const { currentPageNumber: pageNumber, _pages } = PDFViewerApplication.pdfViewer;
       const { viewport } = _pages[pageNumber - 1];
@@ -300,3 +448,159 @@ $('#add-comment-form').on($.modal.AFTER_CLOSE, function() {
   isReply = false;
   submitComment = false;
 });
+
+function getUserInfo() {
+  const userInfoStr = localStorage.getItem('user_info')
+  try {
+    return JSON.parse(userInfoStr)
+  } catch(e) {
+    return null
+  }
+}
+
+const getAnnotationsForAdapter = async (documentId, pageNumber) => {
+  const data = await getAnnotationsReq(documentId);
+  const annotations = data.map(m => JSON.parse(m.location)).filter(f => f.page === pageNumber);
+  return { annotations, documentId, pageNumber };
+}
+
+const addAnnotationReq = async (documentId, pageNumber, annotation, comment) => {
+  console.log('document id', documentId);
+  console.log('page number', pageNumber);
+  console.log('annotation', annotation);
+
+  const user = getUserInfo()
+  if (!user) {
+    throw new Error('user missing');
+  }
+
+  const source = parseInt(localStorage.getItem('source'), 10)
+  if (!source) {
+    throw new Error('data source missing');
+  };
+
+  const body = {
+    user: user.id,
+    dataroom: dataroomId,
+    file: documentId,
+    question: comment,
+    location: JSON.stringify(annotation),
+  };
+
+  const reqDiscussion = await fetch(`${baseUrl}/dataroom/discuss/`, {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'clienttype': '3',
+      'source': source,
+      'token': user.token,
+    },
+    body: JSON.stringify(body),
+  });
+  const response = await reqDiscussion.json();
+  console.log('req discussion', response);
+  const { code } = response;
+  if (code !== 1000) {
+    if (response.errormsg) {
+      alert(response.errormsg);
+    } else {
+      alert('未知错误');
+    }
+  }
+}
+
+const addAnnotationCommentReq = async (annotationId, comment) => {
+  console.log('annotation id', annotationId);
+
+  const user = getUserInfo()
+  if (!user) {
+    throw new Error('user missing');
+  }
+
+  const source = parseInt(localStorage.getItem('source'), 10)
+  if (!source) {
+    throw new Error('data source missing');
+  };
+
+  const body = {
+    answer: comment,
+  };
+
+  const reqDiscussion = await fetch(`${baseUrl}/dataroom/discuss/${annotationId}/`, {
+    method: 'PUT',
+    credentials: 'include',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'clienttype': '3',
+      'source': source,
+      'token': user.token,
+    },
+    body: JSON.stringify(body),
+  });
+  const response = await reqDiscussion.json();
+  console.log('req discussion', response);
+  const { code } = response;
+  if (code !== 1000) {
+    if (response.errormsg) {
+      alert(response.errormsg);
+    } else {
+      alert('未知错误');
+    }
+  } else {
+    return response;
+  }
+}
+
+const deleteAnnotationReq = async id => {
+  const user = getUserInfo()
+  if (!user) {
+    throw new Error('user missing');
+  }
+
+  const source = parseInt(localStorage.getItem('source'), 10)
+  if (!source) {
+    throw new Error('data source missing');
+  };
+
+  const reqDiscussion = await fetch(`${baseUrl}/dataroom/discuss/${id}/`, {
+    method: 'DELETE',
+    credentials: 'include',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'clienttype': '3',
+      'source': source,
+      'token': user.token,
+    },
+  });
+  const response = await reqDiscussion.json();
+  console.log('req discussion', response);
+  const { code } = response;
+  if (code !== 1000) {
+    if (response.errormsg) {
+      alert(response.errormsg);
+    } else {
+      alert('未知错误');
+    }
+  } else {
+    return response;
+  }
+}
+
+const testAnnotation = {
+  class: "Annotation",
+  height: 52.2,
+  page: 1,
+  type: "area",
+  uuid: "d76646cb-d364-46d8-9983-56a4c88fd3b5",
+  width: 87.00000000000003,
+  x: 84.04545454545455,
+  y: 36.13636363636365,
+};
+
+// getAnnotations(documentId, 1);
+
+// addAnnotation(documentId, 1, testAnnotation);
